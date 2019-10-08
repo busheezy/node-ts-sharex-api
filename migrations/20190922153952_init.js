63991a3afcebc68f72524a904172ca40f16c0ea4
Ryan Bucshon <busheezy@gmail.com>
2019-10-07 20:55:34 -0500

    add start of migrations

--- migrations/20190922153952_init.js
+++ migrations/20190922153952_init.js
@@ -1,10 +1,68 @@
-exports.up = async knex => {
-  await knex.schema.createTable('', table => {
-    table.increments('id').primary();
-    table.string('', 255);
-  });
+exports.up = knex => {
+  return knex.schema
+    .createTable('files', table => {
+      table.increments('id').primary();
+      table.string('fileId', 255);
+    })
+    .createTable('images', table => {
+      table.increments('id').primary();
+      table.string('fileId', 255);
+    })
+    .createTable('links', table => {
+      table.increments('id').primary();
+      table.string('url', 255);
+    })
+    .createTable('pastes', table => {
+      table.increments('id').primary();
+      table.text('content');
+    })
+    .createTable('shares', table => {
+      table.increments('id').primary();
+
+      table
+        .integer('fileId')
+        .unsigned()
+        .references('id')
+        .inTable('files')
+        .onDelete('CASCADE')
+        .index();
+
+      table
+        .integer('imageId')
+        .unsigned()
+        .references('id')
+        .inTable('images')
+        .onDelete('CASCADE')
+        .index();
+
+      table
+        .integer('linkId')
+        .unsigned()
+        .references('id')
+        .inTable('links')
+        .onDelete('CASCADE')
+        .index();
+
+      table
+        .integer('pasteId')
+        .unsigned()
+        .references('id')
+        .inTable('pastes')
+        .onDelete('CASCADE')
+        .index();
+
+      table
+        .dateTime('createdAt')
+        .notNullable()
+        .defaultTo(knex.fn.now());
+    });
 };
 
-exports.down = async knex => {
-  await knex.schema.dropTableIfExists('');
+exports.down = knex => {
+  return knex.schema
+    .dropTableIfExists('shares')
+    .dropTableIfExists('files')
+    .dropTableIfExists('images')
+    .dropTableIfExists('links')
+    .dropTableIfExists('pastes');
 };

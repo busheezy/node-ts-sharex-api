b53276f44bc339f81af65748e48d2ca97ad857ab
Ryan Bucshon <busheezy@gmail.com>
2019-10-10 11:38:14 -0500

    make this more defined

--- src/randomString.ts
+++ src/randomString.ts
@@ -1,7 +1,11 @@
 import cryptoRandomString from 'crypto-random-string';
 
 function randomString(): string {
-  return cryptoRandomString({ length: 6, type: 'base64' });
+  return cryptoRandomString({
+    length: 6,
+    characters:
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+  });
 }
 
 export default randomString;
